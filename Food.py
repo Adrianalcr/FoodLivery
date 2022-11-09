829826de39c3997753b55d15f2e938fc84db38e2
Adriana Lima <adrianalima_webdesigner@outlook.com>
2022-11-09 15:33:47 -0300

    Update Food.py

--- Food.py
+++ Food.py
@@ -72,14 +72,28 @@ class Food:
         sf.loginf2 = Frame(sf.scr, height=618,width=1366)
         sf.c = Canvas(sf.loginf2, height=618,width=1366)        
         sf.c.pack()
+    
+        sf.logo2 = PhotoImage(file="pizza3.png")        
+        sf.c.create_image(683,309,image=sf.logo2)
 
-        #sf.back = PhotoImage(file="pizza4.png")        
-        #sf.c.create_image(683,284,image=sf.back)
+        sf.c.create_rectangle(50,100,700,450,fill="#d3ede6",outline="white",width=5)
+        sf.log=Label(sf.loginf2,text="Administração: Login",fg="white",bg="#0b1335",width=31,font=("cooper black",26))
+        sf.log.place(x=59,y=105) 
 
+        sf.lab1=Label(sf.loginf2,text="Usuário :",bg="#d3ede6",font=("cooper black",22))
+        sf.lab1.place(x=100,y=180)        
+        sf.user=Entry(sf.loginf2,bg="white",font=("cooper black",22),bd=1,justify='left')
+        sf.user.place(x=320,y=180)     
 
-        #sf.logo2 = PhotoImage(file="pizza3.png")        
-        #sf.c.create_image(683,309,image=sf.logo2)
+        sf.lab2=Label(sf.loginf2,text="Senha :",bg="#d3ede6",font=("cooper black",22))
+        sf.lab2.place(x=105,y=250) 
+        sf.pasd=Entry(sf.loginf2,bg="white",font=("cooper black",22),bd=1,justify='left')
+        sf.pasd.place(x=320,y=250) 
 
+        sf.lg = Button(sf.loginf2,text="ENTRAR",fg="white",bg="black",cursor='hand2',font=("coooper black", 20),bd=1)
+        sf.lg.place(x=400,y=320)    
+        sf.loginf2.pack(fill=BOTH,expand=1)   
+        sf.scr.mainloop()     
 
 x=Food()
 x.main()
